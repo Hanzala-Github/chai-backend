efe91e84e2f2f089b8d80e8ae71857809d02a758
Hanzala <han.zala010104@gmail.com>
2024-10-02 21:52:42 +0500

    utils code disscussion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+import { app } from "./app.js";
 import connectDB from "./db/db.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(`Error`, error);
+    });
+
+    // listening on port
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`* Server is runung on port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo db connection failed !!! ", error);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+//***This is the first approach to create the asyncHandler with the help of trycatch */
+
+// const asyncHandler = (requestHandler) => async (req, res, next) => {
+//   try {
+//     await requestHandler(req, res, next);
+//   } catch (error) {
+//     res
+//       .status(error.code || 500)
+//       .json({ success: false, message: error.message });
+//   }
+// };
+
+// ****This is the second approach to create the asyncHandler with the help of promise**//
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };
