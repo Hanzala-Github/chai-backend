7bed9a6d742f4d87f8181a47329a45f1c12d0f42
Hanzala <han.zala010104@gmail.com>
2024-10-03 15:03:03 +0500

    cloudinary file uploading code and multer file store in distStorage

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Corrected to use CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) throw new Error("localFilePath is not avalible");
+
+    // upload the file on cloudinary
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // file has been uploaded successfully
+
+    console.log("file has been uploaded successfully ", response.url);
+    return response;
+  } catch (error) {
+    console.log("Error during upload : ", error.message);
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
